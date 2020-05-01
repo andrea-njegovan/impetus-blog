03f2ae73c1a5e1b79dec8f58e3bd03509984d04d
Andrea Njegovan <andrea.njegovan@kindredgroup.com>
2020-05-01 12:31:24 +0200

    styling search

--- src/components/Search/Search.style.js
+++ src/components/Search/Search.style.js
@@ -11,19 +11,37 @@ export const SearchWrapper = styled.div`
         width: 100%;
         > .ais-SearchBox-form {
             width: 100%;
-            > button {
+            margin-bottom: 0.45rem;
+            > .ais-SearchBox-reset {
+                display: none;
+            }
+            > .ais-SearchBox-submit {
                 display: none;
                 ${respondTo.sm`
                     display: inline-block;
+                    border-top-right-radius: 0.5rem; 
+                    border-bottom-right-radius: 0.5rem;
+                    border-color: ${color.secondary};
+                    border-left: none;
+                    border-width: 0.05rem;
+                    padding: 0.03rem 0.5rem 0.04rem 0.5rem;
                 `}
             }
             > .ais-SearchBox-input {
                 width: 80%;
                 height: 4rem;
                 width: 100%;
+                color: ${color.primary};
+                font-size: 18px;
                 ${respondTo.sm`
                     height: auto;
                     width: auto;
+                    border-right: none;
+                    border-top-left-radius: 0.5rem; 
+                    border-bottom-left-radius: 0.5rem;
+                    border-color: ${color.secondary};
+                    border-width: 0.05rem;
+                    padding-left: 0.5rem;
                 `}  
             }
         }
